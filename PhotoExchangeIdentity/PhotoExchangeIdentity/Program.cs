44d649c804624498ef813be30332d4bafb244ee2
Egor Makarov <egor.makarov@cogniteq.com>
2022-04-04 17:46:47 +0300

    Add identity server configuration

--- PhotoExchangeIdentity/PhotoExchangeIdentity/Program.cs
+++ PhotoExchangeIdentity/PhotoExchangeIdentity/Program.cs
@@ -1,16 +1,44 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using PhotoExchangeIdentity;
+using PhotoExchangeIdentity.Data;
+using PhotoExchangeIdentity.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 
 builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<AppUser>()
     .AddInMemoryApiResources(Configuration.ApiResources)
     .AddInMemoryIdentityResources(Configuration.IdentityResources)
     .AddInMemoryApiScopes(Configuration.ApiScopes)
     .AddInMemoryClients(Configuration.Clients)
     .AddDeveloperSigningCredential();
 
+builder.Services.AddDbContext<AuthDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+builder.Services.AddIdentity<AppUser, IdentityRole>(config =>
+    {
+        config.Password.RequiredLength = 4;
+        config.Password.RequireDigit = false;
+        config.Password.RequireNonAlphanumeric = false;
+        config.Password.RequireUppercase = false;
+    })
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
+
+
+builder.Services.ConfigureApplicationCookie(config =>
+{
+    config.Cookie.Name = "PhotoExchange.Cookie";
+    config.LoginPath = "/Auth/Login";
+    config.LogoutPath = "/Auth/Logout";
+});
 
 
 
